916effd9e81ba3feee69c7a9fdc07090cba069fc
葛会军 <bgenius@sina.com>
2019-04-29 13:45:46 +0800

    config集成eureka

--- configTest/src/main/java/com.test/BootApplication.java
+++ configTest/src/main/java/com.test/BootApplication.java
@@ -3,6 +3,7 @@ package com.test;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -15,6 +16,7 @@ import org.springframework.web.bind.annotation.RestController;
  **/
 @SpringBootApplication
 @RestController
+@EnableEurekaClient
 public class BootApplication {
     public static void main(String[] args) {
         SpringApplication.run(BootApplication.class,args);
